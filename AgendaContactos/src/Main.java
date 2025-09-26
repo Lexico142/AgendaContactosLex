9ac802cb20621a2a0499519e48f7543d2778fd34
Lex-ico <teralex142@gmail.com>
2025-09-26 10:00:42 +0200

    v2
    actividad 1

--- AgendaContactos/src/Main.java
+++ AgendaContactos/src/Main.java
@@ -1,5 +1,20 @@
+
+import java.util.Scanner;
+
 public class Main{
     public static void main(String[] args) {
-        System.out.println("hola");
+
+        Scanner src = new Scanner(System.in);
+
+        System.out.println("Elige con un número:");
+        System.out.println("1. Añadir contacto");
+        System.out.println("2. Mostrar contactos");
+        System.out.println("3. Buscar contacto");
+        System.out.println("4. Salir");
+
+        String opcion = src.nextLine();
+
+        System.out.println("Has elegido la opción '" + opcion + "'");
+
     }
 }
