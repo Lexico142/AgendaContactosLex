0d82c84c70b69316b0a994b03578af7096a28c15
Lex-ico <teralex142@gmail.com>
2025-10-01 08:48:45 +0200

    deholaaa

---
+++ entornos/src/Main.java
@@ -0,0 +1,5 @@
+public class Main{
+    public static void main(String[] args) {
+        System.out.println("esta es una prueba para añadir un archivo a una repo de github mediante comandos");
+    }
+}
