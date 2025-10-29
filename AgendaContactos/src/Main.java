223bf6e3270dcd9f6a59543178ecde155e3b68fe
Lex-ico <teralex142@gmail.com>
2025-10-29 12:30:42 +0100

    upd 29/10

--- AgendaContactos/src/Main.java
+++ AgendaContactos/src/Main.java
@@ -1,42 +1,60 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         Scanner src = new Scanner(System.in);
+        boolean activo = true;
+        String numero = "waza";
+        String nombre = "";
 
+        while (activo) {
         System.out.println("Elige con un número:");
         System.out.println("1. Añadir contacto");
         System.out.println("2. Mostrar contactos");
         System.out.println("3. Buscar contacto");
         System.out.println("4. Salir");
-
+        System.out.print("Opción: ");
         int opcion = src.nextInt();
+
         src.nextLine();
 
-        switch(opcion){
-            case 1:
-                while (true){
-                    boolean numerovalido = false;
 
-                    if (!numerovalido){
+            switch (opcion) {
+                case 1:
+                    while (true) {
                         System.out.println("Introduce el número del contacto que quieras añadir");
-                        String numero = src.nextLine();
+                        numero = src.nextLine();
 
-                        if (numero.length() == 9){
+                        if (numero.length() == 9) {
                             System.out.println("Introduce el nombre y apellidos del contacto que quieras añadir");
-                            String nombre = src.nextLine();
-                            System.out.println("Has añadido al contacto " + nombre + " correctamente");
-                            numerovalido = true;
+                            nombre = src.nextLine();
+                            System.out.println("Has añadido al contacto " + nombre + " correctamente\n");
+                            Thread.sleep(1000);
                             break;
                         } else {
                             System.out.println("Introduce un número de teléfono válido");
                         }
                     }
+                    break;
 
+                case 2:
+                    System.out.println("Has elegido 'Mostrar contactos'");
+                    System.out.println("A continuación se imprimirán todos tus contactos");
+                    System.out.println("Nombre: " + nombre);
+                    System.out.println("Número: " + numero + "\n");
 
-                }
-
+                    Thread.sleep(1000);
+                    break;
+                case 3:
+                    System.out.println("Has elegido 'Buscar contacto'");
+                    System.out.print("Buscar: ");
+                    break;
+                case 4:
+                    System.out.println("Has elegido 'salir', hasta pronto!");
+                    activo = false;
+                    break;
+            }
         }
     }
 }
